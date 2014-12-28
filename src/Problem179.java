9170a4ba144ee9ff15c5bddd472fcfdecc931a2b
Ankeet Parikh <ankeet@Ankeets-MacBook-Pro.local>
2014-12-28 01:27:11 -0500

    More consise

--- src/Problem179.java
+++ src/Problem179.java
@@ -2,14 +2,17 @@
 public class Problem179 {
 	public static void main(String [] args)
 	{
-		int r = (int)1e7;
+		int r = (int)1e7, n=0;
 		int[] d = new int[r+1];
 		for(int i = 1; i<d.length; i++)
+		{
 			for(int j = i; j<d.length; j+=i)
 				d[j]++;
-		int n =0;
-		for(int i=1; i<d.length-1; i++)
-			if(d[i] == d[i+1]) n++;
+			if(i>2)
+				if(d[i] == d[i-1])
+					n++;
+		}
+		
 		System.out.println(n);
 		
 	}
