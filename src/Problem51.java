3712711568d2d1b8de40d22119a3a8eb6f339545
Ankeet Parikh <ankeet.parikh96@gmail.com>
2015-01-04 19:16:02 -0500

    Still incomplete, but added a little more

--- src/Problem51.java
+++ src/Problem51.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+
 
 public class Problem51 {
-	static int[] primes;
+	static int[] primes = new int[0]; //size is arbitrary, will be changed
 	public static void main(String [] args)
 	{
-		int range = (int)1e7;
-		int [][] a = replist(8,4,null);
-		System.out.println(a.length);
-		Print2d(a);
+		int range = (int)1e2; //the upper bound is just a guess
+		primeSieve(range);
+		//for each prime, we will determine
+		
+		
+		
 		
 	}
 	
@@ -76,27 +80,64 @@ public class Problem51 {
 		return n;
 	}
 	
+	public static void primeSieve(int n) //generates a list of primes up until n and stores them in primes
+	{
+		boolean[] p = new boolean[n+1];
+		for(int i=0; i<p.length; i++)p[i] =true;
+		p[0] = false;
+		p[1] = false;
+		for(int i=2; i<p.length; i++)
+		{
+			for(int j = i*2; j<p.length; j+=i)
+			{
+				p[j] = false;
+			}
+		}
+		ArrayList<Integer> pr = new ArrayList<Integer>(0); //will store the primes, but we don't know how many there are
+		for(int i=0; i<p.length; i++)
+			if(p[i])
+				pr.add(i);
+		primes = new int[pr.size()];
+		for(int i=0; i<primes.length; i++)
+			primes[i] = pr.get(i);
+		
+		
+	}
 	public static boolean isPrime(long n)
 	{
-		double rt = Math.sqrt(n);
-		for(int i=2; i<=rt; i++)
-			if(n%i==0)
+		/*
+		 * Here, we optimize trial division by using the list of primes.
+		 * This only works if the number does not exceed the maximum value in primes
+		 */
+		for(int i= 0; i<primes.length; i++)
+		{
+			int p = primes[i];
+			if(p>Math.sqrt(n))
+				return true;
+			if(n%p == 0)
 				return false;
+		}
 		return true;
+		
 	}
-	
-	public static void Print2d(int[][] a)
+	public static void Print1d(int[] a)
 	{
-		System.out.print("{ ");
+		System.out.print("{");
 		for(int i=0; i<a.length; i++)
 		{
-			System.out.print("{");
-			for(int j =0; j<a[i].length; j++)
-			{
-				String s = j==a[i].length-1? ""+a[i][j]: a[i][j] +",";
-				System.out.print(s);
-			}
-			System.out.print("} ");
+			if(i<a.length - 1) System.out.print(a[i] +",");
+			else System.out.print(a[i]);
+		}
+		System.out.print("}");
+		
+	}
+	public static void Print2d(int[][] a)
+	{
+		System.out.print("{");
+		for(int i=0; i<a.length; i++)
+		{
+			Print1d(a[i]);
+			if(i<a.length-1) System.out.print(",");
 		}
 		System.out.println("}");
 	}
